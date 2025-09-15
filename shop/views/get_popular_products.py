a27266d9ab2415ae73cf76be2a09354a767c5e26
Sardorbek <nsardorbek776@gmail.com>
2025-09-15 17:45:38 +0500

    ok

--- shop/views/get_popular_products.py
+++ shop/views/get_popular_products.py
@@ -10,6 +10,25 @@ from shop.views.get_all_products_list import ProductsSerializer
 class PopularProducts(APIView):
 
     def get(self, request):
+        lang = request.query_params.get("lang", "uz")
         popular_products = Products.objects.order_by("-created_datetime")[:8]
-        serializer = ProductsSerializer(popular_products, many=True)
+        products_data=[]
+        for product in popular_products:
+
+            product_data = {
+                "id": product.id,
+                "name": product.name if lang == "uz" else product.name_ru if lang == "ru" else product.name_en,
+                "category": product.category.name if lang == "uz" else product.category.name_ru if lang == "ru" else product.category.name_en,
+                "price": product.price,
+                "quantity": product.quantity,
+                "discount": product.discount,
+                "video_url": product.video_url,
+                "discounted_price": product.discounted_price,
+                "average_rating": product.average_rating,
+                "description": product.description if lang == "uz" else product.description_ru if lang == "ru" else product.description_en,
+                "images": [ image.image.url for image in product.images.all()]
+            }
+            products_data.append(product_data)
+
+        serializer = ProductsSerializer(products_data, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
