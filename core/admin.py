3a13644ed92cd4af3179afdb05595e1b772f165e
Sardorbek <nsardorbek776@gmail.com>
2025-09-15 14:14:27 +0500

    ok

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
+from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken, OutstandingToken
 # Default User va Group'ni admindan olib tashlash
 admin.site.unregister(User)
 admin.site.unregister(Group)
+admin.site.unregister(BlacklistedToken)
+admin.site.unregister(OutstandingToken)
