d5ba38e9721f207273c024ada387a9868c28d06a
Sardorbek <nsardorbek776@gmail.com>
2025-09-17 15:09:08 +0500

    ok

--- core/constants.py
+++ core/constants.py
@@ -6,6 +6,6 @@ CLICK_MERCHANT_ID = config("CLICK_MERCHANT_ID")
 
 click_up = ClickUp(service_id=CLICK_SERVICE_ID, merchant_id=CLICK_MERCHANT_ID)
 class CustomPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 8
     page_size_query_param = 'page_size'
     max_page_size = 1000
