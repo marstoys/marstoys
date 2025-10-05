5eeb778ac52e1c2e00a0c388be381b7685225497
Sardorbek <nsardorbek776@gmail.com>
2025-10-05 10:07:09 +0500

    ok

--- orders_bot/handlers/start.py
+++ orders_bot/handlers/start.py
@@ -36,7 +36,7 @@ async def process_order_number(message: Message, state: FSMContext):
             f"🏠 Manzil: {order.ordered_by.address}\n"
             f"💳 To'lov usuli: {order.payment_method.capitalize()}\n"
             f"💳 To'langanligi : {"Tolangan" if order.is_paid else "To'lanmagan" }\n"
-            f"📦 Buyurtma holati: {order.status}\n"
+            f"📦 Buyurtma holati: {order.get_status_display()}\n"
             f"🕒 Sana: {timezone.localtime(order.created_datetime).strftime('%Y-%m-%d %H:%M')}"
             f"\n\n📦 Buyurtma tafsilotlari:\n"
         )
@@ -66,6 +66,6 @@ async def order_status_handler(callback_query: CallbackQuery):
         order.status = new_status
         order.save()
         await callback_query.message.edit_text(text="Assalomu alaykum. Bu bot sizga Buyurtmalarni avtomatik yuborib boradi.",reply_markup=main_keyboard())
-        await callback_query.answer(text=f"Buyurtma holati '{order.status}' ga o'zgartirildi.", show_alert=True)
+        await callback_query.answer(text=f"Buyurtma holati '{order.get_status_display()}' ga o'zgartirildi.", show_alert=True)
     except Order.DoesNotExist:
         await callback_query.answer(text="Buyurtma topilmadi.", show_alert=True)
