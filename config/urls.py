9e996f65c4d6fc1a2bc0a3228bb93b105d13faa5
Sardorbek <nsardorbek776@gmail.com>
2025-10-06 16:01:48 +0500

    ok

--- config/urls.py
+++ config/urls.py
@@ -51,4 +51,7 @@ urlpatterns = [
 def custom_page_not_found(request, exception):
     return render(request, '404.html', status=404)
 
+def custom_server_error(request):
+    return render(request, '500.html', status=500)
+
 handler404 = 'config.urls.custom_page_not_found'
