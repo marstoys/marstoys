0919212074fd9443148100243d1439e36693a598
Sardorbek Norqulov <152975289+Norqulov-Sardorbek@users.noreply.github.com>
2025-12-01 14:15:00 +0500

    Update constants.py

--- core/constants.py
+++ core/constants.py
@@ -10,7 +10,7 @@ ESKIZ_PASSWORD = config("ESKIZ_PASSWORD")
 
 click_up = ClickUp(service_id=CLICK_SERVICE_ID, merchant_id=CLICK_MERCHANT_ID)
 class CustomPagination(PageNumberPagination):
-    page_size = 8
+    page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 1000
     
