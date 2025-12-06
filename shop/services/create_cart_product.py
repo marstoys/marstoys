91044eede5072341a299751cb5b2d035d7794e6c
Sardorbek <nsardorbek776@gmail.com>
2025-12-06 11:02:51 +0500

    ok

--- shop/services/create_cart_product.py
+++ shop/services/create_cart_product.py
@@ -6,14 +6,11 @@ from core.exceptions.error_messages import ErrorCodes
 def create_cart_product(user_id, data):
     product_id = data.get('product_id')
     quantity = data.get('quantity')
-    color_display = data.get('color')
 
     if not product_id:
         raise CustomApiException(ErrorCodes.INVALID_INPUT, message="Product ID is required.")
     if quantity is None:
         raise CustomApiException(ErrorCodes.INVALID_INPUT, message="Quantity is required.")
-    if not color_display:
-        raise CustomApiException(ErrorCodes.INVALID_INPUT, message="Color is required.")
 
     try:
         product = Products.objects.get(id=product_id)
