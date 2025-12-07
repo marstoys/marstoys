4a59e6c923db2e52c3086dfe3424cb760f8dad9b
Sardorbek <nsardorbek776@gmail.com>
2025-12-07 18:17:23 +0500

    ok

--- shop/services/update_cart_product.py
+++ shop/services/update_cart_product.py
@@ -11,10 +11,8 @@ def update_cart(user_id,data):
         raise CustomApiException(ErrorCodes.NOT_FOUND, message="The specified cart product does not exist.")
     if quantity is None:
         raise CustomApiException(ErrorCodes.INVALID_INPUT, message="Quantity is required.")
-    new_quantity = quantity + cart.quantity
-    if new_quantity<=0:
-        raise CustomApiException(ErrorCodes.INVALID_INPUT, message="Quantity must be greater than zero.")
-    cart.quantity = new_quantity
+    
+    cart.quantity = quantity
     cart.save()
     return True
     
