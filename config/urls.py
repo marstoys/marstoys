2e3b3abcf4f93eedcba130730f2265d493bf11d5
Sardorbek <nsardorbek776@gmail.com>
2025-12-15 18:23:41 +0500

    ok

--- config/urls.py
+++ config/urls.py
@@ -39,8 +39,8 @@ urlpatterns = [
                 path('admin/', admin.site.urls),
                 path('shop/', include('shop.urls')),
                 path('users/', include('users.urls')),
-                path('docs<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-                path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-                path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+                path('swaggercha<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+                path('swaggercha/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+                path('redoccha/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
                 path("payment/click/update/", ClickWebhookAPIView.as_view()),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
