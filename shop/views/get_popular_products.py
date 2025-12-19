b1637713a60c64f771945e444a2389e98b7608ac
Sardorbek <nsardorbek776@gmail.com>
2025-12-19 10:18:21 +0500

    ok

--- shop/views/get_popular_products.py
+++ shop/views/get_popular_products.py
@@ -15,7 +15,7 @@ class PopularProducts(APIView):
     )
 
     def get(self, request):
-        popular_products = Products.objects.order_by("-created_datetime").prefetch_related('images').select_related('category').filter(quantity__gt=0)[:30]
+        popular_products = Products.objects.order_by("-created_datetime").prefetch_related('images').select_related('category').filter(quantity__gt=0)[:28]
         products_data=[]
         for product in popular_products:
             
